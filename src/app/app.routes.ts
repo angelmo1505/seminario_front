2495a053d452ad900f569bb1e8c8490e3641feb4
Miguel Montes <angelmo1505@gmail.com>
2025-04-27 01:10:02 -0500

    menu navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { InventoryComponent } from './components/inventory/inventory.component';
+import { ProductComponent } from './components/product/product.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
 export const routes: Routes = [
-    { path: '', component: InventoryComponent}
+    { path: '', component: InventoryComponent},
+    { path: 'inventory', component: InventoryComponent},
+    { path: 'product', component: ProductComponent},
+    { path: 'sales', component: SalesComponent},
+    { path: 'customers', component: CustomersComponent},
 ];
